90ad19497d22a81a148de983f0fbb2362db2d8a0
Vorravoot Nilprapan <vnilprap@z2t9c1.42bangkok.com>
2022-03-21 19:04:03 +0700

    master

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: vnilprap <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/13 10:04:57 by vnilprap          #+#    #+#             */
-/*   Updated: 2022/03/20 06:16:52 by vnilprap         ###   ########.fr       */
+/*   Updated: 2022/03/21 19:03:47 by vnilprap         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <fcntl.h>
@@ -14,15 +14,15 @@
 #include <string.h>
 #include "get_next_line.h"
 
-static	int	ft_getpos(char *s1, char c)
+static	int	ft_chknl(char *s)
 {
 	int	i;
 
 	i = 0;
-	while (s1[i] && s1[i] != c)
+	while (s[i] && s[i] != '\n')
 		i++;
-	if (i == (int)ft_strlen(s1))
-		return (-1);
+	if (i == (int)ft_strlen(s))
+		i++;
 	return (i);
 }
 
@@ -31,46 +31,52 @@ char	*get_next_line(int fd)
 	int	rd;
 	char	*s;
 	char	*bf;
-	static char *sub;
+	char	*tmp;
+	static	char	*sub;
 
-	s = 0;
 	rd = 1;
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (0);
-	bf = malloc(sizeof(char) * (BUFFER_SIZE + 1));
-	if (!bf)
-		 return (0);
+	s = 0;
+	tmp = 0;
 	while (rd > 0)
 	{
-		rd = read(fd, bf, BUFFER_SIZE);
-		bf[rd] = 0;
-		printf("read %d bytes\n", rd);
+		bf = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+		if (!bf)
+			return (0);
 		if (sub != 0)
 		{
-			free(s);
-			s = ft_strjoin(s, sub);
+			s = strdup(sub);
 			free(sub);
+			sub = 0;
+			printf("left : %s\n", s);
 		}
-		if (ft_getpos(bf, '\n') >= 0)
+		rd = read(fd, bf, BUFFER_SIZE);
+		if (rd > 0)
 		{
-			if (ft_strlen(bf) != 1)
+			bf[rd] = 0;
+			if (ft_chknl(bf) < (int)ft_strlen(bf))
 			{
-				printf("there\n");
-				sub = ft_substr(bf, ft_getpos(bf, '\n') + 1, rd);
-				bf = ft_substr(bf, 0, ft_getpos(bf, '\n') + 1);
+				sub = ft_substr(bf, 0, ft_chknl(bf) + 1);
+				tmp = ft_strjoin(s, sub);
+				free(sub);
+				free(s);
+				s = tmp;
+				if ((int)ft_strlen(bf) - ft_chknl(bf) > 0)
+					sub = ft_substr(bf, ft_chknl(bf) + 1, ft_strlen(bf) - ft_chknl(bf));
+				free(bf);
+				return (s);
 			}
-			s = ft_strjoin(s, bf);
-			free(bf);
-			return (s);
+			else
+			{
+				tmp = ft_strjoin(s, bf);
+				free(s);
+				s = tmp;
+			}
 		}
-		s = ft_strjoin(s, bf);
+		free(bf);
 	}
-	free(bf);
-	if (ft_strlen(s) > 0)
-	{
-		printf("here\n");
+	if (s != 0 && ft_strlen(s) > 0)
 		return (s);
-	}
-	free(s);
 	return (0);
 }
