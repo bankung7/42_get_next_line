5f2ae16e6dee0944c0b54e6a69eff2b34869452c
Vorravoot Nilprapan <vnilprap@z2t9c1.42bangkok.com>
2022-03-22 17:42:03 +0700

    master

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: vnilprap <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/13 10:04:57 by vnilprap          #+#    #+#             */
-/*   Updated: 2022/03/21 19:03:47 by vnilprap         ###   ########.fr       */
+/*   Updated: 2022/03/22 17:26:51 by vnilprap         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <fcntl.h>
@@ -19,8 +19,15 @@ static	int	ft_chknl(char *s)
 	int	i;
 
 	i = 0;
-	while (s[i] && s[i] != '\n')
+	while (s[i])
+	{
+		if (s[i] == '\n')
+		{
+			i++;
+			return (i);
+		}
 		i++;
+	}
 	if (i == (int)ft_strlen(s))
 		i++;
 	return (i);
@@ -32,51 +39,52 @@ char	*get_next_line(int fd)
 	char	*s;
 	char	*bf;
 	char	*tmp;
-	static	char	*sub;
+	static	char 	*sub;
 
+	s = 0;
 	rd = 1;
+	bf = 0;
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (0);
-	s = 0;
-	tmp = 0;
+	if (sub != 0 && ft_strlen(sub) > 0)
+	{
+		s = ft_strdup(sub);
+		free(sub);
+		sub = 0;
+	}
 	while (rd > 0)
 	{
 		bf = malloc(sizeof(char) * (BUFFER_SIZE + 1));
 		if (!bf)
 			return (0);
-		if (sub != 0)
-		{
-			s = strdup(sub);
-			free(sub);
-			sub = 0;
-			printf("left : %s\n", s);
-		}
 		rd = read(fd, bf, BUFFER_SIZE);
 		if (rd > 0)
 		{
 			bf[rd] = 0;
-			if (ft_chknl(bf) < (int)ft_strlen(bf))
+			tmp = ft_strjoin(s, bf);
+			free(s);
+			s = tmp;
+			if (ft_chknl(s) < (int)ft_strlen(s))
 			{
-				sub = ft_substr(bf, 0, ft_chknl(bf) + 1);
-				tmp = ft_strjoin(s, sub);
-				free(sub);
-				free(s);
-				s = tmp;
-				if ((int)ft_strlen(bf) - ft_chknl(bf) > 0)
-					sub = ft_substr(bf, ft_chknl(bf) + 1, ft_strlen(bf) - ft_chknl(bf));
+				tmp = ft_substr(s, 0, ft_chknl(s));
+				sub = ft_substr(s, ft_chknl(s), (int)ft_strlen(s) - ft_chknl(s));
 				free(bf);
-				return (s);
-			}
-			else
-			{
-				tmp = ft_strjoin(s, bf);
 				free(s);
-				s = tmp;
+				return (tmp);
 			}
 		}
 		free(bf);
 	}
 	if (s != 0 && ft_strlen(s) > 0)
+	{
+		if (ft_chknl(s) < (int)ft_strlen(s))
+		{
+			tmp = ft_substr(s, 0, ft_chknl(s));
+			sub = ft_substr(s, ft_chknl(s), ft_strlen(s) - ft_chknl(s));
+			free(s);
+			s = tmp;
+		}
 		return (s);
+	}
 	return (0);
 }
