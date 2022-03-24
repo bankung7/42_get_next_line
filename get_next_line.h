b0c808b551cb2b746148b81f2f772d842760e3be
Vorravoot Nilprapan <vnilprap@z3t8c1.42bangkok.com>
2022-03-24 17:51:43 +0700

    norm clear

--- get_next_line.h
+++ get_next_line.h
@@ -6,21 +6,21 @@
 /*   By: vnilprap <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/13 20:55:14 by vnilprap          #+#    #+#             */
-/*   Updated: 2022/03/19 15:02:49 by vnilprap         ###   ########.fr       */
+/*   Updated: 2022/03/24 17:42:00 by vnilprap         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#ifndef GNL_H
-# define GNL_H
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
-#include <unistd.h>
-#include <unistd.h>
-#include <stdlib.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
 
 char	*get_next_line(int fd);
-void	*ft_memmove(void *dst, const void *src, size_t len);
-size_t	ft_strlen(char const *s);
 char	*ft_strdup(char const *s1);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
+void	*ft_memmove(void *dst, const void *src, size_t len);
+size_t	ft_strlen(char const *s);
 
 #endif
